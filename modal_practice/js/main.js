b2884c229c51184b23919c2bd7cab922bbe77bda
ichinosekiwa <128582009+ichinosekiwa@users.noreply.github.com>
2023-10-17 08:29:28 +0900

    Update main.js

--- modal_practice/js/main.js
+++ modal_practice/js/main.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const openModal = document.querySelector('.open-btn');
+const modalWindow = document.querySelector('.modal');
+
+openModal.addEventListener('click', function (){
+    modalWindow.classList.add('active')
+});
