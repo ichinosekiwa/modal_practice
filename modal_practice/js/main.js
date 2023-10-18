ca4f461855a31fe88e36ce05879f48c597cc38da
ichinosekiwa <128582009+ichinosekiwa@users.noreply.github.com>
2023-10-19 00:19:49 +0900

    モーダル領域外クリックが閉じない

--- modal_practice/js/main.js
+++ modal_practice/js/main.js
@@ -1,8 +1,24 @@
 'use strict';
 
-const openModal = document.querySelector('.open-btn');
+const openButton = document.querySelector('.open-btn');
 const modalWindow = document.querySelector('.modal');
+const decision = document.querySelector('.modal-btn');
+const mainContainer = document.querySelector('container');
 
-openModal.addEventListener('click', function (){
-    modalWindow.classList.add('active')
+function openModal() {
+    modalWindow.classList.add('active');
+}
+
+openButton.addEventListener('click', openModal);
+
+function closeModal() {
+    modalWindow.classList.remove('active');
+}
+
+decision.addEventListener('click', () => {
+    closeModal();
+});
+mainContainer.addEventListener('click', (event) => {
+   if(event.target === mainContainer) { closeModal();
+   }
 });
