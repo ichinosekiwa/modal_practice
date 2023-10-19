41d0456084c19fa769a459b4dac8378346414906
ichinosekiwa <128582009+ichinosekiwa@users.noreply.github.com>
2023-10-19 21:43:44 +0900

    js/css 修正

--- modal_practice/js/main.js
+++ modal_practice/js/main.js
@@ -3,22 +3,23 @@
 const openButton = document.querySelector('.open-btn');
 const modalWindow = document.querySelector('.modal');
 const decision = document.querySelector('.modal-btn');
-const mainContainer = document.querySelector('container');
+const mainContainer = document.querySelector('.container');
 
+//modalをアクティブにして開く
 function openModal() {
     modalWindow.classList.add('active');
 }
-
 openButton.addEventListener('click', openModal);
 
+//modalを閉じる
 function closeModal() {
     modalWindow.classList.remove('active');
 }
-
+//決定ボタンで閉じる
 decision.addEventListener('click', () => {
     closeModal();
 });
-mainContainer.addEventListener('click', (event) => {
-   if(event.target === mainContainer) { closeModal();
-   }
+//modal領域外で閉じる
+mainContainer.addEventListener('click', () => {
+    closeModal();
 });
