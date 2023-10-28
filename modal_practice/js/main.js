090ed4b03bc124e42114fa03effe09469f43629d
ichinosekiwa <128582009+ichinosekiwa@users.noreply.github.com>
2023-10-28 11:06:26 +0900

    モーダルの切り替え、

--- modal_practice/js/main.js
+++ modal_practice/js/main.js
@@ -3,16 +3,27 @@
 const openButton = document.querySelector('.open-btn');
 const modalWindow = document.querySelector('.modal');
 const modalBox = document.querySelector('.modal-box');
+const nextBtn = document.querySelector('.modal-btnNext');
 const decision = document.querySelector('.modal-btn');
 const mainContainer = document.querySelector('.container');
+const modalAge = document.getElementById('modal-age');
+const modalArea = document.getElementById('modal-area');
 // 選択した値をブラウザに表示
 const radioAge = document.getElementsByName('age');
+const radioArea = document.getElementsByName('area');
 
 //modalを開く
 function openModal() {
     modalWindow.classList.add('active');
 }
 openButton.addEventListener('click', openModal);
+
+// 次へクリックでmodalを切り替える
+function nextModal() {
+    modalAge.classList.remove('is-show');
+    modalArea.classList.add('is-show');
+}
+nextBtn.addEventListener('click', nextModal);
 
 //modalを閉じる
 function closeModal() {
@@ -27,25 +38,31 @@ decision.addEventListener('click', () => {
 modalWindow.addEventListener('click', () => {
     closeModal();
 });
+
+// ラジオボタンの値を取り出す
+function buttonClick() {
+    let checkValueAge = '';
+    let checkValueArea = '';
+
+    radioAge.forEach((radio) => {
+        if (radio.checked) {
+            checkValueAge = radio.value;
+        }
+    });
+
+    radioArea.forEach((radio) => {
+        if (radio.checked) {
+            checkValueArea = radio.value;
+        }
+    });
+
+    const resultItem = document.getElementById('result');
+    resultItem.textContent = `${checkValueAge}　${checkValueArea}`;
+}
+const checkButton = document.getElementById('checkButton');
+checkButton.addEventListener('click', buttonClick);
+
 //modal-box バブリングを止める
 modalBox.addEventListener("click", function(e) {
     e.stopPropagation();
 });
-
-// ラジオボタンの値を取り出す
-function buttonClick() {
-    let checkValue = '';
-
-    radioAge.forEach((radio) => {
-        if (radio.checked) {
-            checkValue = radio.value;
-        }
-    });
-    const resultItem = document.getElementById('result');
-    resultItem.textContent = checkValue;
-}
-let checkButton = document.getElementById('checkButton');
-checkButton.addEventListener('click', buttonClick);
-
-
-
